71fed7540a3e24b482c23ddbd855144fb39f19b3
Michael Walle <michael.walle@kontron.com>
2015-05-28 15:06:43 +0200

    simplify parse_idx test case
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- utest/test_utils.py
+++ utest/test_utils.py
@@ -2,22 +2,7 @@ from nose.tools import raises
 from SnmpLibrary import utils
 
 def test_parse_idx():
-    idx = "10.20.30"
-    t = utils.parse_idx(idx)
-    assert isinstance(t, tuple)
-    assert t == (10, 20, 30)
-
-    idx = 10
-    t = utils.parse_idx(idx)
-    assert isinstance(t, tuple)
-    assert t == (10,)
-
-    idx = [10, 20, 30]
-    t = utils.parse_idx(idx)
-    assert isinstance(t, tuple)
-    assert t == (10, 20, 30)
-
-    idx = (10, 20, 30)
-    t = utils.parse_idx(idx)
-    assert isinstance(t, tuple)
-    assert t == (10, 20, 30)
+    assert utils.parse_idx('1.2.3') == (1, 2, 3)
+    assert utils.parse_idx(1) == (1,)
+    assert utils.parse_idx([1, 2, 3]) == (1, 2, 3)
+    assert utils.parse_idx((1, '2', 3)) == (1, 2, 3)
