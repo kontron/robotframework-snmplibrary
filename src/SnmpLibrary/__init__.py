30cdf656edc60a3b6d705c0a789c6d9d22a6c632
Michael Walle <michael.walle@kontron.com>
2015-05-29 13:43:29 +0200

    update copyright
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- src/SnmpLibrary/__init__.py
+++ src/SnmpLibrary/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Kontron Europe GmbH
+# Copyright 2014-2015 Kontron Europe GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
